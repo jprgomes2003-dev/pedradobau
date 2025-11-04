c75cb9dc52d02ca5b19878ddec39b067ce313cbb
joao barata <jpr.gomes2003@gmail.com>
2025-11-04 08:29:09 -0300

    Página Contatos Pronta

--- assets/JavaScript/menu.js
+++ assets/JavaScript/menu.js
@@ -24,28 +24,3 @@ function toggleMenu() {
   });
 });
 
-function toggleMenu() {
-  const menu = document.querySelector("nav.mobile");
-  const overlay = document.getElementById("mobile-overlay");
-
-  menu.classList.toggle("open");
-  document.body.classList.toggle("menu-open");
-}
-
-
-  document.addEventListener("DOMContentLoaded", () => {
-  const btnFechar = document.querySelector(".fechar-menu");
-  const menu = document.querySelector("nav.mobile");
-
-  btnFechar.addEventListener("click", () => {
-    menu.classList.remove("open");
-    document.body.classList.remove("menu-open");
-  });
-
-  // Também pode fechar clicando no overlay:
-  const overlay = document.getElementById("mobile-overlay");
-  overlay.addEventListener("click", () => {
-    menu.classList.remove("open");
-    document.body.classList.remove("menu-open");
-  });
-});
